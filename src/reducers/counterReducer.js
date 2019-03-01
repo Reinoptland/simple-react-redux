05ae21f7a0a589c7bb8528223cdacee8fa11f547
reinoptland <reinoptland@gmail.com>
2019-03-01 08:49:18 +0100

    Debug it ex2

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,8 +1,5 @@
 export default (state = 0, action) => {
     switch (action.type) {
-        case 'INCREMENT':
-            return state + 1
-    
         default:
             return state
     }
--- src/reducers/lightswitchReducer.js
+++ src/reducers/lightswitchReducer.js
@@ -1,8 +1,5 @@
 export default (state = true, action) => {
     switch (action.type) {
-        case 'TOGGLE_LIGHTSWITCH':
-            return !state 
-    
         default:
             return state
     }
