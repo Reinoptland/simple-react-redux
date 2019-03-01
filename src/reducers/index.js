95d87b8d72f047a961170a1a6a6aece0b55ce128
reinoptland <reinoptland@gmail.com>
2019-03-01 08:59:13 +0100

    Debug it ex5

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
-import counter from './counterReducer'
 import lightswitch from './lightswitchReducer'
 
 export default combineReducers({
-    counter,
     lightswitch
 })
